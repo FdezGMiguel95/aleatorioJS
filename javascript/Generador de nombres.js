ef1224a203624965bdb8235e7687c8987ac2dd77
Miguel Fernández Guerrero <fdezguerrerom@gmail.com>
2025-11-09 15:04:25 +0100

    Creado generador de nombres y ajustado temporizador

--- javascript/Generador de nombres.js
+++ javascript/Generador de nombres.js
@@ -1,2 +1,30 @@
-// Script para Generador de nombres.html
-console.log('Archivo Generador de nombres.js cargado correctamente');
+document.addEventListener('DOMContentLoaded', () => {
+    const nombres = {
+        masculino: [
+            "Alejandro", "Miguel", "Javier", "Daniel", "Hugo", 
+            "Pablo", "Lucas", "Adrián", "Marco", "David"
+        ],
+        femenino: [
+            "Sofía", "Lucía", "Martina", "María", "Paula",
+            "Sara", "Carla", "Valeria", "Alba", "Elena"
+        ],
+        neutro: [
+            "Río", "Mar", "Luz", "Sol", "Cielo", 
+            "Alma", "Paz", "Kai", "Azul", "Indigo"
+        ]
+    };
+
+    const botonGenerar = document.getElementById('generarBoton');
+    const resultadoParrafo = document.getElementById('nombreResultado');
+    const formulario = document.getElementById('generadorForm');
+
+    botonGenerar.addEventListener('click', () => {
+        const generoSeleccionado = formulario.querySelector('input[name="genero"]:checked').value;
+        const listaNombres = nombres[generoSeleccionado];
+        const indice = Math.floor(Math.random() * listaNombres.length);
+        const nombreGenerado = listaNombres[indice];
+        resultadoParrafo.textContent = nombreGenerado;
+        
+    });
+    document.head.appendChild(style);
+});
